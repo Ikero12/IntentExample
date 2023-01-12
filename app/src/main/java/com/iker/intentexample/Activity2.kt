43e70e5a3a8e3f052ce239a6a02d5b2633583dc7
Iker <irodrigueziglesias@danielcastelao.org>
2023-01-12 14:31:55 +0100

    Ambos Ejercicios

--- app/src/main/java/com/iker/intentexample/Activity2.kt
+++ app/src/main/java/com/iker/intentexample/Activity2.kt
@@ -1,10 +1,13 @@
 package com.iker.intentexample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class Activity2 : AppCompatActivity() {
 
@@ -13,18 +16,40 @@ class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity2)
-
         btn2 = findViewById(R.id.button2)
 
-        val intent = Intent(this, MainActivity::class.java)
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val intent = getIntent()
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
-        btn2.setOnClickListener(object : View.OnClickListener {
+        val valor = intent.getIntExtra("Proveedores",0)
+        val nroClientes = findViewById<TextView>(R.id.textView2)
+
+        nroClientes.text = valor.toString()
+
+        //Botón para cambiar a primera actividad
+       /* btn2.setOnClickListener(object : View.OnClickListener {
             override fun onClick(view: View?) {
+
                 startActivity(intent)
             }
 
-        })
+        })*/
 
+
+        intent.putExtra("Saludo","Hola")
+        intent.putExtra("Saludo2","Chau")
+        //debug
+        Log.d("CORRECTO: ","Actualizado Intent")
+
+        // Configuro el result para que la Main lo tenga disponible
+        // Posibles resultados:
+        //    Activity.RESULT_OK
+        //    Activity.RESULT_CANCELED
+        setResult(Activity.RESULT_OK, intent)
+
+        //debug
+        Log.d("CORRECTO: ","Actualizado Resultado")
+        //Cierro la activity
+        finish()
     }
 }
--- app/src/main/java/com/iker/intentexample/MainActivity.kt
+++ app/src/main/java/com/iker/intentexample/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.iker.intentexample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import java.time.Instant
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btn1:Button
+    val RESULTADO_UNO=1
+    val RESULTADO_DOS=2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +23,34 @@ class MainActivity : AppCompatActivity() {
         btn1 = findViewById(R.id.button)
         val intent = Intent(this, Activity2::class.java)
 
-
-        intent.putExtra("key","value")
-        intent.putExtra("key2","value2")
-
-
-
         btn1.setOnClickListener{
-            startActivity(intent)
+            intent.putExtra("Proveedores",2)
+            intent.putExtra("Clientes",3)
+
+            startActivityForResult(intent,RESULTADO_UNO)
+            startActivityForResult(intent,RESULTADO_DOS)
         }
 
 
     }
-}
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val saludo = findViewById<TextView>(R.id.textView)
+        if(resultCode != Activity.RESULT_OK) return
+        when(requestCode) {
+            RESULTADO_UNO -> {
+                if (data != null) {
+                    saludo.text = data.getStringExtra("Saludo")
+                }; }
+            RESULTADO_DOS ->{
+                if (data != null) {
+                    data.getStringExtra("Saludo2")?.let { Log.d("Mensaje:" , it) }
+                };
+            }
+            // Other result codes
+            else -> {}
+        }
+}}
